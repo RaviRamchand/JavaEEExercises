3a2e328af01b627ef169cf0a62ff6441c4e0ba7f
Ravi <raviramchand8@gmail.com>
2022-02-16 14:59:32 -0500

    main method

---
+++ FormBindingEx/src/main/java/ca/ravi/formbindingex/FormBindingExApplication.java
@@ -0,0 +1,13 @@
+package ca.ravi.formbindingex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormBindingExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FormBindingExApplication.class, args);
+    }
+
+}
