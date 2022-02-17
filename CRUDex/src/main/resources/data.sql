8d03a4b88157daec367fef4202522d4c2b007151
Ravi <raviramchand8@gmail.com>
2022-02-17 16:05:11 -0500

    dummy records for Appointment table

---
+++ CRUDex/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO Appointment(FirstName, Email, AppointmentDate, AppointmentTime)
+VALUES('Harry', 'harry@harry.com', '2020-09-01', '12:00:00'),
+      ('Hermione', 'hermione@hermione.com', '2020-08-20', '15:00:00'),
+      ('Ron', 'ron@ron.com', '2020-08-20', '15:20:00');
