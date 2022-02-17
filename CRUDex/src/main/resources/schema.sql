3b3749e2fbc4a3421fff724e5e66eae4bd35e425
Ravi <raviramchand8@gmail.com>
2022-02-17 16:04:52 -0500

    Appointment table

---
+++ CRUDex/src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Appointment(
+    AppointmentId Long PRIMARY KEY AUTO_INCREMENT,
+    FirstName VARCHAR(30),
+    Email VARCHAR(35),
+    AppointmentDate DATE,
+    AppointmentTime TIME
+);
