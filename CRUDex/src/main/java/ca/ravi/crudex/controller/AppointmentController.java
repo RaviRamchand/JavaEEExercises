5adf60b1d24ef06dcba1447d7f8bbd5ad1eb1ce6
Ravi <raviramchand8@gmail.com>
2022-02-18 15:26:03 -0500

    mapping for delete appointment

--- CRUDex/src/main/java/ca/ravi/crudex/controller/AppointmentController.java
+++ CRUDex/src/main/java/ca/ravi/crudex/controller/AppointmentController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -25,6 +26,12 @@ public class AppointmentController {
     public String getNewAppointment(Model model, @ModelAttribute Appointment appointment){
         da.insertAppointment(appointment);
         model.addAttribute("appointment", new Appointment());
+        return "redirect:/";
+    }
+
+    @GetMapping("/deleteAppointmentById/{id}")
+    public String deleteAppointmentById(@PathVariable Long id){
+        da.deleteId(id);
         return "redirect:/";
     }
 }
