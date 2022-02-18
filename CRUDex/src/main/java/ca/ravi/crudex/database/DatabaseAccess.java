c15592ffb258cdb95cd4595f80d37c2210dbc2b2
Ravi <raviramchand8@gmail.com>
2022-02-18 15:24:52 -0500

    comments and delete method

--- CRUDex/src/main/java/ca/ravi/crudex/database/DatabaseAccess.java
+++ CRUDex/src/main/java/ca/ravi/crudex/database/DatabaseAccess.java
@@ -14,12 +14,14 @@ public class DatabaseAccess {
     @Autowired
     protected NamedParameterJdbcTemplate jdbc;
 
+    //Returns Appointment Table to the AppointmentController as a List
     public List<Appointment> getAppointments(){
         MapSqlParameterSource namedParameter = new MapSqlParameterSource();
         String query = "SELECT * FROM Appointment";
         return jdbc.query(query, namedParameter, new BeanPropertyRowMapper<>(Appointment.class));
     }
 
+    //Inserts appointment information from user into table
     public void insertAppointment(Appointment appointment){
         MapSqlParameterSource namedParameter = new MapSqlParameterSource();
         String query = "INSERT INTO Appointment(FirstName, Email, AppointmentDate, AppointmentTime)" +
@@ -28,6 +30,15 @@ public class DatabaseAccess {
         namedParameter.addValue("Email", appointment.getEmail());
         namedParameter.addValue("AppointmentDate", appointment.getAppointmentDate());
         namedParameter.addValue("AppointmentTime", appointment.getAppointmentTime());
+
+        jdbc.update(query, namedParameter);
+    }
+
+    //Deletes a specific appointment using the id
+    public void deleteId(Long id){
+        MapSqlParameterSource namedParameter = new MapSqlParameterSource();
+        String query = "DELETE FROM Appointment WHERE id = :id";
+        namedParameter.addValue("id", id);
 
         jdbc.update(query, namedParameter);
     }
